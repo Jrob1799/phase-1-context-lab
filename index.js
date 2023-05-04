26e9add10187b4c88163a43f3f402976061b93a7
Jacob <jacobwroberts33@gmail.com>
2023-05-04 15:22:17 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
-/* Your Code Here */
+// Your Code Here
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+}
+
+function createEmployeeRecords(arr) {
+    const result = arr.map(record => {
+        return createEmployeeRecord(record)
+    })
+    return result
+}
+
+function createTimeInEvent(date) {
+    this.timeInEvents.push(createTimeEvent(date, 'TimeIn'))
+    return this
+};
+
+function createTimeEvent(date, type) {
+    return {
+        type,
+        hour: parseInt(date.split(" ")[1]),
+        date: date.split(" ")[0]
+    }
+}
+
+
+
+function createTimeOutEvent(date) {
+    this.timeOutEvents.push(createTimeEvent(date, 'TimeOut'))
+    return this
+};
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(record => record.date === date)
+    const timeOut = this.timeOutEvents.find(record => record.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName);
+}
+
+function calculatePayroll(records) {
+    return records.reduce((total, record) => total + allWagesFor.call(record), 0);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
